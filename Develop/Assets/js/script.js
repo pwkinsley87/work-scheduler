d9a365c6ec7b8f23bc4f844055a2105c34ef2c50
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-07-31 10:38:58 -0500

    rewrote script.js. date/time now displaying in header

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -1,35 +1,25 @@
-//display current day  in 
-var currentDay = $('#currentDay');
-var containerEl = $('.container');
-
-// use moment to display current date 
-var hourEl = moment().hour();
-
-// display all working hours in an array in hA format to show AM or PM
-var workingHours = [
-    moment().hour(9).format('hA'),
-    moment().hour(10).format('hA'),
-    moment().hour(11).format('hA'),
-    moment().hour(12).format('hA'),
-    moment().hour(1).format('hA'),
-    moment().hour(2).format('hA'),
-    moment().hour(3).format('hA'),
-    moment().hour(4).format('hA'),
-    moment().hour(5).format('hA')
-];
+tasks = [];
 
 
-// add task to scheduler 
-var addTask = function(taskText) {
-    // create elements that make up a task item 
-
+// load tasks 
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"))
+    if(!tasks) {
+        tasks = [];
+    } ; 
+    printTasks(tasks)
 }
 
-// test test test 
 
-//edit task 
-var editTask
+// print tasks 
+var printTasks = function() {
+    $.each(tasks, function(list, arr){
 
+        var taskP = $("<p>").addClass("description task-item-" + list).text(arr)
 
-// delete task 
-var deleteTask 
+        $("#task-item-" + list).replaceWith(taskP);
+    })
+}
+
+var Today = (moment().format("MMMM D, YYYY"))
+    $("#currentDay").text(Today);
