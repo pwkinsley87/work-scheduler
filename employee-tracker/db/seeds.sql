c586f3ad0e47e2478436bcc14ea5f6de51576daa
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-08-02 11:54:16 -0500

    Revert "Merge branch 'main' of github.com:pwkinsley87/profileGenerator2"
    This reverts commit bc89425c2d0f507c50e85251a5d578f99a4c7d46, reversing
    changes made to 98c8090fe96c11644f40466ca7fa86ceda87c189.

--- employee-tracker/db/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO department (name) 
-VALUES ("Sales"), ("Production"), ("Logistics"), ("Maintenance"), ("Warehouse");
-
-INSERT INTO role (title, salary, department_id)
-VALUE ("Sales Associate", 55000.00, 1), ("Press Operator", 34500.00, 2), ("Driver", 28500.00, 4), ("Maintenance Associate - 3rd Shift", 43200.00, 5), ("Warehouse Associate - A Shift", 33000.00, 3);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUE ("Leopold", "Bloom", 2, 1), ("Stephen", "Dedalus", 2, 2), ("Buck", "Mulligan", 2, 3), ("Martin", "Cunningham", 3, 5), ("Paddy", "Dignam", 2, 3);
